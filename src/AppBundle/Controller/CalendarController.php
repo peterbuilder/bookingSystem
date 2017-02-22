7fa0fb20f8d5acebec8b2a819e8e8d65565df4c7
peterbuilder <peterbuilder@github.com>
2017-02-22 14:03:43 +0100

    First commit

---
+++ src/AppBundle/Controller/CalendarController.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 21.02.17
+ * Time: 13:11
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class CalendarController extends Controller
+{
+    /**
+     * @Route("/{month}/{year}")
+     * @Template("Calendar/showCalendar.html.twig")
+     */
+    public function getParametersAction($month, $year)
+    {
+        //Number of days in month
+        $numberOfDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+
+        //Name of first day in week. If sunday: $dayOfWeek = 7
+        $date = gregoriantojd($month, 1, $year);
+        if(jddayofweek($date, 0) != 0) {
+            $dayOfWeek = jddayofweek($date, 0);
+        }
+        else {
+            $dayOfWeek = 7;
+        }
+
+        //Number of weeks in month
+        $numberOfWeeks = $this->numOfWeeks($month, $year);
+
+        return [
+            "numberOfDays" => $numberOfDays,
+            "dayOfWeek" => $dayOfWeek,
+            "month" => $month,
+            "year" => $year,
+            "numberOfWeeks" =>$numberOfWeeks - 1
+        ];
+    }
+
+    /**
+     * @Route("/show/{day}/{month}/{year}")
+     */
+    public function showDayAction($day, $month, $year)
+    {
+        return new Response("Dzień: " . $day . " Miesiąc: " . $month . " Rok: " . $year);
+    }
+
+    //Count weeks in month
+    private function numOfWeeks($month, $year)
+    {
+        $numberOfDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        $numberOfWeeks = 0;
+        $day = 1;
+
+        for($day; $day <= $numberOfDays; $day++) {
+            $date = gregoriantojd($month, $day, $year);
+            if(jddayofweek($date, 0) == 0) {
+                $numberOfWeeks++;
+            }
+        }
+
+        //If the last day of week isn't sunday: $numberOfWeeks++
+        if(jddayofweek($date, 0) != 0) {
+            $numberOfWeeks++;
+        }
+
+        return $numberOfWeeks;
+    }
+
+    private function generateCalendarToAjaxAction()
+    {
+
+    }
+}
+
