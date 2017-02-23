1fbb7c8f3c3fa270abdffefa590794706f29d76d
peterbuilder <peterbuilder@github.com>
2017-02-23 21:30:49 +0100

    Add getting data from showDay.html.twig

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -10,6 +10,9 @@ namespace AppBundle\Controller;
 
 
 
+use AppBundle\Entity\Task;
+use DateInterval;
+use DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -26,9 +29,62 @@ class TaskController extends Controller
      */
     public function addTaskAction(Request $request)
     {
+        $this->denyAccessUnlessGranted('ROLE_USER', null, 'Access denied');
+
         $serviceType = $request->request->get('serviceType');
+        $start = $request->request->get('startTime');
+        $date = $request->request->get('date');
+        $date = $this->checkDateFormat($date);
+        $startTime = $date . ' ' . $start . ':00';
+        $time = new DateTime($startTime);
 
+        switch ($serviceType) {
+            case 'manHaircut':
+                    $time->add(new DateInterval('PT15M'));
+                    $endTime = $time->format('Y-m-d H:i:s');
+                break;
 
-        return ['service' => $serviceType];
+            case 'womanHaircut':
+                    $time->add(new DateInterval('PT30M'));
+                    $endTime = $time->format('Y-m-d H:i:s');
+                break;
+
+            case 'permanent':
+                    $time->add(new DateInterval('PT1H30M'));
+                    $endTime = $time->format('Y-m-d H:i:s');
+                break;
+        }
+
+        $this->setTask($serviceType, $startTime, $endTime);
+        return ['endTime' => $startTime,'date'=>$date];
+    }
+
+    //Change day and month format to 0x-0x
+    private function checkDateFormat($date)
+    {
+        $dateArray = explode('-', $date);
+        foreach ($dateArray as $key => $element) {
+            if(strlen($element) == 1) {
+                $element = '0' . $element;
+                $dateArray[$key] = $element;
+            }
+        }
+        $date = implode('-', $dateArray);
+
+        return $date;
+    }
+
+    private function setTask($serviceType, $startTime, $endTime)
+    {
+        $task = new Task();
+        $task->setUser($this->getUser());
+        $task->setUserId($this->getUser()->getId());
+        $task->setType($serviceType);
+        $task->setStart($startTime);
+        $task->setEnd($endTime);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($task);
+        $em->flush();
     }
 }
