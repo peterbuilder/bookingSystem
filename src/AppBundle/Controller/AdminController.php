9d462653142481a5a9a0f10c2e04dc80b276f459
peterbuilder <peterbuilder@github.com>
2017-03-06 11:05:14 +0100

    Add controller getNotAcceptedTasksAction()

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -1,15 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: piotr
- * Date: 03.03.17
- * Time: 18:19
- */
 
 namespace AppBundle\Controller;
 
 
-class AdminController
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminController extends Controller
 {
+    /**
+     * @Route("/notAcceptedTask")
+     * @Template()
+     */
+    public function getNotAcceptedTasksAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $tasks = $em->getRepository('AppBundle:Task')->findBy(array('isAccepted' => 0));
+
+        return ['tasks' => $tasks];
+    }
+
+
 
 }
